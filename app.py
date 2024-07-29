72babb5dd210e9f1c0887acb2094cef464194198
RichardPZ <139816817+RichardOPZ@users.noreply.github.com>
2024-07-29 01:49:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-$ls -a .git .gitignore .streamlit app.py README.md requirements.txt
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
+
